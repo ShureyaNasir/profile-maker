c4477adbe01eddda925cea2e19f700a1409f80a8
ShureyaNasir <shureyanasir1994@gmail.com>
2022-03-25 11:21:17 +0000

    added some data

--- src/components/ProfileForm.js
+++ src/components/ProfileForm.js
@@ -32,25 +32,26 @@ const handleForm =(e)=>{
 
          <label>
          First Name
-         <input name='firstname' type="text" onChange={handler}/>
+         <input name='firstname'value={profile.firstname} type="text" onChange={handler}/>
          </label>
          
          
          <label>
          Last Name
-         <input name='lastname' type="text" onChange={handler}/>
+         <input name='lastname'value={profile.lastname} type="text" onChange={handler}/>
          </label>
-
+         </div>
          
+         <div className="names">
          <label>
          Email
-         <input name='email' type="email" onChange={handler}/>
+         <input name='email'value={profile.email} type="email" onChange={handler}/>
          </label>
 
          
          <label>
          Phone
-         <input name='phone' type="tel" onChange={handler}/>
+         <input name='phone'value={profile.phone} type="tel" onChange={handler}/>
          </label>
         </div>
       </fieldset>
